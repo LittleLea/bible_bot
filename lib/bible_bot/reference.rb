f23f77fa5bf3dd8d5463ff22341e2ef0117a54ec
Derik Olsson <do@derik.co>
2018-11-08 14:26:20 -0600

    Shift to a param, much cleaner.

--- lib/bible_bot/reference.rb
+++ lib/bible_bot/reference.rb
@@ -40,17 +40,26 @@ module BibleBot
       end
     end
 
-    def formatted
+    def formatted(always_include_chapter = false)
       # if start and end chapters are the same
       if chapter_number == end_chapter_number || end_chapter_number.nil?
-        # If the start verse is one and the end verse is the last verse in the chapter
-        if verse_number == 1 && end_verse_number == book.chapters[chapter_number-1]
-          return "#{book.formatted_name} #{chapter_number}"
-        # If start and end verses are the same
-        elsif verse_number == end_verse_number || end_verse_number.nil?
-          return "#{book.formatted_name} #{chapter_number}:#{verse_number}"
-        else
-          return "#{book.formatted_name} #{chapter_number}:#{verse_number}-#{end_verse_number}"
+        if book.chapters.length == 1 && always_include_chapter == false # single chapter book
+          # If start and end verses are the same
+          if verse_number == end_verse_number
+            return "#{book.formatted_name} #{verse_number}"
+          else
+            return "#{book.formatted_name} #{verse_number}-#{end_verse_number}"
+          end
+        else # multichapter book
+          # If the start verse is one and the end verse is the last verse in the chapter
+          if verse_number == 1 && end_verse_number == book.chapters[chapter_number-1]
+            return "#{book.formatted_name} #{chapter_number}"
+          # If start and end verses are the same
+          elsif verse_number == end_verse_number || end_verse_number.nil?
+            return "#{book.formatted_name} #{chapter_number}:#{verse_number}"
+          else
+            return "#{book.formatted_name} #{chapter_number}:#{verse_number}-#{end_verse_number}"
+          end
         end
       else # start and end chapters are different
         return "#{book.formatted_name} #{chapter_number}:#{verse_number}-#{end_chapter_number}:#{end_verse_number}"
