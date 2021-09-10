455a401b0d0bfb2df53a69b96a7056c2301f13e3
Jeff McFadden <jeff@mcfadden.io>
2021-09-10 09:55:13 -0700

    Initial support for deuterocanonical works. The regexps still need a bit of work.

--- lib/bible_bot.rb
+++ lib/bible_bot.rb
@@ -1,3 +1,4 @@
+require "ostruct"
 require "bible_bot/version"
 require "bible_bot/book"
 require "bible_bot/bible"
@@ -5,3 +6,38 @@ require "bible_bot/errors"
 require "bible_bot/verse"
 require "bible_bot/reference"
 require "bible_bot/reference_match"
+
+module BibleBot
+  DEFAULTS = {
+    include_deuterocanonical_content: false
+  }
+
+  def self.options
+    @options ||= OpenStruct.new(DEFAULTS.dup)
+  end
+
+  def self.options=(opts)
+    @options = opts
+  end
+
+  ##
+  # Configuration for BibleBot, use like:
+  #
+  #   BibleBot.configure do |config|
+  #     config.include_deuterocanonical_content = true
+  #   end
+  def self.configure
+    yield(options)
+  end
+
+  def self.include_deuterocanonical_content?
+    !!self.options.include_deuterocanonical_content
+  end
+
+  def self.include_deuterocanonical_content=(inc)
+    self.options.include_deuterocanonical_content = (inc === true)
+
+    # We need to reset the stored regexps because they take content into account.
+    BibleBot::Bible.reset_regular_expressions
+  end
+end
--- lib/bible_bot/bible.rb
+++ lib/bible_bot/bible.rb
@@ -534,8 +534,176 @@ module BibleBot
       )
     ]
 
+    @@deuterocanonical_books = [
+      Book.new(
+        id: 101,
+        name: "Tobit",
+        abbreviation: "Tob",
+        regex: "Tob(?:it)?",
+        testament: "",
+        chapters: [22, 14, 17, 21, 22, 18, 17, 21, 6, 14, 18, 22, 18, 15],
+      ),
+      Book.new(
+        id: 102,
+        name: "Judith",
+        abbreviation: "Jth",
+        regex: "(?:Ju?d?i?th?)",
+        testament: "",
+        chapters: [16, 28, 10, 15, 24, 21, 32, 36, 14, 23, 23, 20, 20, 19, 14, 25],
+      ),
+      Book.new(
+        id: 103,
+        name: "Additions to Esther",
+        abbreviation: "Add Esth",
+        regex: "(?:((Add\s?|(The\s)?Rest\sof\s|A))Est?h?e?r?)",
+        testament: "",
+        chapters: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 8, 30, 16, 24, 10],
+      ),
+      Book.new(
+        id: 104,
+        name: "Wisdom of Solomon",
+        abbreviation: "Wis",
+        regex: "(?:(Wi?sd?\.?(\sof\s)?(Sol|om)?))",
+        testament: "",
+        chapters: [16, 24, 19, 20, 23, 25, 30, 21, 18, 21, 26, 27, 19, 31, 19, 29, 21, 25, 22],
+      ),
+      Book.new(
+        id: 105,
+        name: "Sirach", # a.k.a. Ecclesiasticus
+        abbreviation: "Sir",
+        regex: "Sir(?:ach)?",
+        testament: "",
+        chapters: [29, 18, 30, 31, 17, 37, 36, 19, 18, 30, 34, 18, 25, 27, 20, 28, 27, 33, 26, 30, 28, 27, 27, 31, 25, 20, 30, 26, 28, 25, 31, 24, 33, 26, 24, 27, 30, 34, 35, 30, 24, 25, 35, 23, 26, 20, 25, 25, 16, 29, 30],
+      ),
+      Book.new(
+        id: 106,
+        name: "Ecclesiasticus", # a.k.a. Sirach
+        abbreviation: "Ecclus",
+        regex: "(?:(Eccle?u?s))",
+        testament: "",
+        chapters: [29, 18, 30, 31, 17, 37, 36, 19, 18, 30, 34, 18, 25, 27, 20, 28, 27, 33, 26, 30, 28, 27, 27, 31, 25, 20, 30, 26, 28, 25, 31, 24, 33, 26, 24, 27, 30, 34, 35, 30, 24, 25, 35, 23, 26, 20, 25, 25, 16, 29, 30],
+      ),
+      Book.new(
+        id: 107,
+        name: "Baruch",
+        abbreviation: "Bar",
+        regex: "Bar(?:uch)?",
+        testament: "",
+        chapters: [22, 35, 38, 37, 9, 72],
+      ),
+      Book.new(
+        id: 108,
+        name: "Letter of Jeremiah", # Often placed as Baruch 6, but sometimes stands alone
+        abbreviation: "Ep Jer",
+        regex: "(?:((Le?t?t?e?r?|Ep)(\.\s|\sof\s)?Jer?))",
+        testament: "",
+        chapters: [73],
+      ),
+      Book.new(
+        id: 109,
+        name: "Song of Three Youths", # An extension of Daniel 3... a.k.a. Prayer of Azariah
+        abbreviation: "Sg of 3 Childr",
+        regex: "(?:(((The\s)?(So?n?g)(\.?\s?o?f?\s?)(the\s)?(3|Thr).*))|((Pr)?.*Aza?r?))",
+        testament: "",
+        chapters: [],
+      ),
+      Book.new(
+        id: 110,
+        name: "Susanna", # A book of Daniel
+        abbreviation: "Sus",
+        regex: "(?:(Sus))",
+        testament: "",
+        chapters: [64],
+      ),
+      Book.new(
+        id: 111,
+        name: "Bel and the Dragon", # A book of Daniel
+        abbreviation: "Bel and Dr",
+        regex: "(?:(Bel))",
+        testament: "",
+        chapters: [42],
+      ),
+      Book.new(
+        id: 112,
+        name: "1 Maccabees",
+        abbreviation: "1 Macc",
+        #regex: "(?:((1s?t?|^I{1}|First)\s?Ma?c?))",
+        regex: "(?:1|I)(?:\\s)?Macc(?:abees)?",
+        testament: "",
+        chapters: [63, 70, 59, 61, 68, 63, 50, 32, 73, 89, 74, 53, 53, 49, 41, 24],
+      ),
+      Book.new(
+        id: 113,
+        name: "2 Maccabees",
+        abbreviation: "2 Macc",
+        regex: "(?:((2n?d?|^I{2}|Second)\s?Ma?c?))",
+        testament: "",
+        chapters: [36, 32, 40, 50, 27, 31, 42, 36, 29, 38, 38, 46, 26, 46, 39],
+      ),
+      Book.new(
+        id: 114,
+        name: "3 Maccabees",
+        abbreviation: "3 Macc",
+        regex: "(?:((3r?d?|^I{3}|Third)\s?Ma?c?))",
+        testament: "",
+        chapters: [29, 33, 30, 21, 51, 41, 23],
+      ),
+      Book.new(
+        id: 115,
+        name: "4 Maccabees",
+        abbreviation: "4 Macc",
+        regex: "(?:((4t?h?|IV|Fourth)\s?Ma?c?))",
+        testament: "",
+        chapters: [35, 24, 21, 26, 38, 35, 23, 29, 32, 21, 27, 19, 27, 20, 32, 25, 24, 24],
+      ),
+      Book.new(
+        id: 116,
+        name: "1 Esdras",
+        abbreviation: "1 Esd",
+        regex: "(?:((1s?t?|^I{1}|First)\s?Esd?))",
+        testament: "",
+        chapters: [58, 30, 24, 63, 73, 34, 15, 96, 55],
+      ),
+      Book.new(
+        id: 117,
+        name: "2 Esdras",
+        abbreviation: "2 Esd",
+        regex: "(?:((2n?d?|^I{2}|Second)\s?Esd?))",
+        testament: "",
+        chapters: [40, 48, 36, 52, 56, 59, 140, 63, 47, 59, 46, 51, 58, 48, 63, 78],
+      ),
+      Book.new(
+        id: 118,
+        name: "Prayer of Manasseh",
+        abbreviation: "Pr of Man",
+        regex: "(?:(Pr?.*Man?))",
+        testament: "",
+        chapters: [15],
+      ),
+      Book.new(
+        id: 119,
+        name: "Additional Psalm",
+        abbreviation: "Add Ps",
+        regex: "(?:(Add.*Ps))",
+        testament: "",
+        chapters: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7],
+      ),
+      Book.new(
+        id: 120,
+        name: "Ode",
+        abbreviation: "Ode",
+        regex: "Ode",
+        testament: "",
+        chapters: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7],
+      ),
+    ]
+
     def self.books
-      @@books
+      if BibleBot.include_deuterocanonical_content?
+        @@books + @@deuterocanonical_books
+      else
+        @@books
+      end
     end
 
     # assemble the book regex
@@ -563,6 +731,12 @@ module BibleBot
            '(?:\s*[:\.]\s*)?' +
            '(?<EndVerseNumber>\d{1,3})?' +
          ')?', book_re_string, book_re_string), Regexp::IGNORECASE)
+    end
+
+    def self.reset_regular_expressions
+      @@book_re_string = nil
+      @@book_re = nil
+      @@scripture_re = nil
     end
   end
 end
